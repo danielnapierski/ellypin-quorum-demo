0d063b13ad0152721d06fd651572b59e97de0423
danielnapierski <danielpnapierski@gmail.com>
2018-07-27 20:06:03 +0000

    Added token deployment to truffle

--- project/migrations/2_deploy.js
+++ project/migrations/2_deploy.js
@@ -1,5 +1,13 @@
 var Math = artifacts.require("Math");
+var SafeMath = artifacts.require("SafeMath");
+var AddressUtils = artifacts.require("AddressUtils");
+var SupportsInterfaceWithLookup = artifacts.require("SupportsInterfaceWithLookup");
+var ERC721Token = artifacts.require("ERC721Token");
 
 module.exports = function(deployer) {
     deployer.deploy(Math);
+    deployer.deploy(SafeMath);
+    deployer.deploy(AddressUtils);
+    deployer.deploy(SupportsInterfaceWithLookup);
+    deployer.deploy(ERC721Token, "HnH000001", "eGBS");
 };
