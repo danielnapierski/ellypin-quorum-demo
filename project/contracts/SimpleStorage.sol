337230f1af5fd3578fa36de0d588d9e6736d35ae
danielnapierski <danielpnapierski@gmail.com>
2018-08-07 20:01:20 +0000

    Added sender to SimpleStorage

--- project/contracts/SimpleStorage.sol
+++ project/contracts/SimpleStorage.sol
@@ -1,23 +1,24 @@
 pragma solidity ^0.4.24;
 
-contract simplestorage {
+contract SimpleStorage {
   uint public storedData;
-  address public owner;
+  address public sender;
 
   constructor(uint initVal) public {
-    owner = msg.sender;
+    sender = msg.sender;
     storedData = initVal;
   }
 
-//  function simplestorage(uint initVal) {
-//    storedData = initVal;
-//  }
-
   function set(uint x) public {
+    sender = msg.sender;
     storedData = x;
   }
 
   function get() public constant returns (uint retVal) {
     return storedData;
+  }
+
+  function sender() public constant returns (address retVal) {
+    return sender;
   }
 }
